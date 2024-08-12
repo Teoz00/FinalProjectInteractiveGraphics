f8b438cf0ab784d954cdc3f18bc0c01e80cd424e
Teoz00 <matteomiletta@hotmail.it>
2024-08-12 13:35:42 +0200

    Minor changes

--- waterParticle.js
+++ waterParticle.js
@@ -1,14 +1,24 @@
 class WaterParticle {
-    constructor() {
-        this.particles = [];
-    }
-
-    addParticle(position){
-        this.particles.push({
-            position: [...position]
-        })
+   constructor(x, y, z) {
+        this.position = { x, y, z };
+        this.velocity = { x: 0, y: 0, z: 0 };
+        this.force = { x: 0, y: 0, z: 0 };
+        this.density = 0;
+        this.pressure = 0;
     }
 }
+
+// Parametri SPH
+const SPH = {
+    REST_DENSITY: 1000,  // Densità di riferimento
+    GAS_CONSTANT: 2000,  // Costante del gas
+    VISCOSITY: 2500,     // Viscosità
+    KERNEL_RADIUS: 1.0   // Raggio del nucleo
+};
+
+
+
+
 
 
 /*
